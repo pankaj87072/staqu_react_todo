3718c54d936f9f071a791f4ae6fb2f716c839f7e
Pankaj Kumar Gupta <viraj0420478@gmail.com>
2025-03-12 10:48:45 +0530

    react todo without redux

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -11,32 +11,34 @@ const LiTags=({id,texts ,setLitext})=>{
     // litext.pop(keys);
   }
   return (
-    <div>
+    <div className='li'>
     <li>{texts}</li>
-    <button onClick={()=>deleteTask()}>Delete</button>
+    <button className='delete' onClick={()=>deleteTask()}>Delete</button>
     </div>
   )
 }
 const Todo = () => {
   const [litext,setLitext]=useState([]);
+  const [Task,setTask]=useState("");
   function pushText(){
-    const Tasks=document.getElementById('TaskInput');
-    console.log(Tasks.value);
-    setLitext((prev)=>([...prev,Tasks.value]));
+    // const Tasks=document.getElementById('TaskInput'); //this is mostly done in javascript but in react we have to use usestate so that consitency maintain and easy to manipulate
+    // console.log(Tasks.value);
+    setLitext((prev)=>([...prev,Task]));
+    setTask("");
   }
   return (
     <div className='main' >
       <h2>Todo WebApp</h2>
       <div className='inputs'>
-      <input type='text' id='TaskInput' placeholder='Enter The Task'/>
+      <input type='text' id='TaskInput' value={Task} placeholder='Enter The Task' onChange={(e)=>setTask(e.target.value)}/>
       <button onClick={()=>{
         pushText();
       }}>Add</button>
       </div>
       <ul id='listOfTasks'>
       {
-        litext.map((e,index)=>{
-          return <LiTags id={index} texts={e} setLitext={setLitext}/> 
+        litext.map((text,index)=>{
+          return <LiTags key={index} id={index} texts={text} setLitext={setLitext}/> 
         })
       }
       </ul>
